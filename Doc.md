fe234aebcb31205eb18601cb5acd491d844b6053
anshulk <akaushik3@gmail.com>
2015-05-20 13:32:56 -0600

    new

---
+++ Doc.md
@@ -0,0 +1,15 @@
+---
+title: "Doc.md"
+author: "Anshul"
+date: "Wednesday, May 20, 2015"
+output: html_document
+---
+
+MT Cars Data Analysis
+This Shiny App utilizes the MT Cars Data base to give you a summary of the data for specific Transmissions and Cylinders.
+
+First you need to choose the Cylinder either 4, 6 or 8 they go up by steps of 2.
+
+Second you need to choose between Automatic or Manual Transmission.
+
+Results that will be displayed will be a Summary of Miles per gallon, standard deviation and data set of the 11 Variables for each of the cars in that category. On the second Plot Tab you will be able to see the plots for each.
--- ui.R
+++ ui.R
@@ -39,10 +39,21 @@ shinyUI(fluidPage(
                     br(),
                     tableOutput("view")
                   ), 
-                  tabPanel("Plot", plotOutput("plot"))
+                  tabPanel("Plot", plotOutput("plot")),                
                   
-                  
-      )
-    )
+    tabPanel("Documentation",
+             mainPanel(
+               helpText("MT Cars Data Analysis
+This Shiny App utilizes the MT Cars Data base to give you a summary of the data for specific Transmissions and Cylinders.
+
+First you need to choose the Cylinder either 4, 6 or 8 they go up by steps of 2.
+
+Second you need to choose between Automatic or Manual Transmission.
+
+Results that will be displayed will be a Summary of Miles per gallon, standard deviation and data set of the 11 Variables for each of the cars in that category. On the second Plot Tab you will be able to see the plots for each.")
+             )
     ))
-  )
+)
+)
+)
+)         
